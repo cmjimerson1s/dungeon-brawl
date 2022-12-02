350aba994bd4dcda44644c08733485f9fcbda6d9
cmjimerson1s <cmjimerson1s@gmail.com>
2022-12-02 19:51:02 +0000

    Add basic script for button presses

--- assets/js/main-script.js
+++ assets/js/main-script.js
@@ -1,1 +1,12 @@
-document.getElementById('test').innerText = "test6564"
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            let num1 = Math.floor(Math.random() * 20) + 1;
+            document.getElementById("warrior-dice-value").textContent = num1;
+})
+}
+})
