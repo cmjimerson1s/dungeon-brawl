f62c40f8f66b1446aa6af29b968d6f632172d138
cmjimerson1s <cmjimerson1s@gmail.com>
2022-12-02 20:03:22 +0000

    Update the buttons to have attack and damage be seperate

--- assets/js/main-script.js
+++ assets/js/main-script.js
@@ -5,8 +5,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
     for (let button of buttons) {
         button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "warrior-roll") {
             let num1 = Math.floor(Math.random() * 20) + 1;
             document.getElementById("warrior-dice-value").textContent = num1;
+        } else {
+            alert("Not that button!")
+        }
 })
 }
 })
