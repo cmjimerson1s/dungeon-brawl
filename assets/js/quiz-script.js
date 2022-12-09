e377869724e6573c960398bdef8057c2f11c2015
cmjimerson1s <cmjimerson1s@gmail.com>
2022-12-09 14:00:22 +0000

    Add questions to script doc

---
+++ assets/js/quiz-script.js
@@ -0,0 +1,92 @@
+const myQuestions = [{
+    id: 0,
+    q: 'Who is considered to be the father of modern fantasy?',
+    a: [{ text: 'J.K. Rowling', isCorrect: false },
+        { text: 'J.R.R. Tolkien', isCorrect: true },
+        { text: 'Ernist Hemingway', isCorrect: false },
+    ]
+
+},
+{
+    id: 1,
+    q: 'In Harry Potter the character Dobby is what sort of creature?',
+    a: [{ text: 'House Elf', isCorrect: true, isSelected: false },
+        { text: 'Goblin', isCorrect: false },
+        { text: 'Witch', isCorrect: false },
+    ]
+
+},
+{
+    id: 2,
+    q: 'In Game of Thrones what is the name of the highest quality steel in the series?',
+    a: [{ text: 'Targaryen', isCorrect: false },
+        { text: 'Adamantium', isCorrect: false },
+        { text: 'Valyrian', isCorrect: true },
+    ]
+
+},
+{
+    id: 3,
+    q: 'Who pulled the sword from the stone?',
+    a: [{ text: 'King Arthur', isCorrect: true },
+        { text: 'Merlin', isCorrect: false },
+        { text: 'Queen Maeve', isCorrect: false },
+    ]
+
+},
+{
+    id: 4,
+    q: 'Gandolf had the title of "the Grey" and what else?',
+    a: [{ text: 'the Blue', isCorrect: false },
+        { text: 'the Green', isCorrect: false },
+        { text: 'the White', isCorrect: true },
+    ]
+
+},
+{
+    id: 5,
+    q: 'A minotaur is a mix between a human and what animal?',
+    a: [{ text: 'Bull', isCorrect: true },
+        { text: 'Horse', isCorrect: false },
+        { text: 'Goat', isCorrect: false },
+    ]
+
+},
+{
+    id: 6,
+    q: 'What weapon does Gimli the dwarf prefer?',
+    a: [{ text: 'Sword', isCorrect: false },
+        { text: 'Axe', isCorrect: true },
+        { text: 'Whip', isCorrect: false },
+    ]
+
+},
+{
+    id: 7,
+    q: 'Who killed Dumbledore?',
+    a: [{ text: 'Harry Potter', isCorrect: false },
+        { text: 'Hagrid', isCorrect: false },
+        { text: 'Snape', isCorrect: true },
+    ]
+
+},
+{
+    id: 8,
+    q: 'The MadHater may invite you to what sort of party?',
+    a: [{ text: 'Frat', isCorrect: false },
+        { text: 'Tea', isCorrect: true },
+        { text: 'New Year Eve', isCorrect: false },
+    ]
+
+},
+{
+    id: 9,
+    q: 'Where can you find Narnia?',
+    a: [{ text: 'In the oven', isCorrect: false },
+        { text: 'In a wardrobe', isCorrect: true },
+        { text: 'On the moon', isCorrect: false },
+    ]
+
+},
+
+]
