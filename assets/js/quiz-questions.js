664a2c1a3f596e260fb3d2346a5d2ad0ce545a86
cmjimerson1s <cmjimerson1s@gmail.com>
2022-12-09 11:30:56 +0000

    Add quiz questions to script

---
+++ assets/js/quiz-questions.js
@@ -0,0 +1,82 @@
+let questions = [
+
+    {
+        question: 'Who is considered to be the father of modern fantasy?',
+        option1: 'J.K. Rowling',
+        option2: 'J.R.R. Tolkien',
+        option3: 'Ernist Hemingway',
+        answer:2
+    },
+
+    {
+        question: 'In Harry Potter the character Dobby is what sort of creature?',
+        option1: 'House Elf',
+        option2: 'Goblin',
+        option3: 'Witch',
+        answer:1
+    },
+
+    {
+        question: 'In Game of Thrones what is the name of the highest quality steel in the series?',
+        option1: 'Targaryen',
+        option2: 'Adamantium',
+        option3: 'Valyrian',
+        answer:3
+    },
+
+    {
+        question: 'Who pulled the sword from the stone?',
+        option1: 'King Arthur',
+        option2: 'Merlin',
+        option3: 'Queen Maeve',
+        answer:1
+    },
+
+    {
+        question: 'Gandolf had the title of "the Grey" and what else?',
+        option1: 'the Blue',
+        option2: 'the Green',
+        option3: 'the White',
+        answer:3
+    },
+
+    {
+        question: 'Where can you find Narnia?',
+        option1: 'In the oven',
+        option2: 'In a wardrobe',
+        option3: 'On the moon',
+        answer:2
+    },
+
+    {
+        question: 'A minotaur is a mix between a human and what animal?',
+        option1: 'Bull',
+        option2: 'Horse',
+        option3: 'Goat',
+        answer:1
+    },
+
+    {
+        question: 'What weapon does Gimli the dwarf prefer?',
+        option1: 'Sword',
+        option2: 'Axe',
+        option3: 'Whip',
+        answer:2
+    },
+
+    {
+        question: 'The MadHater may invite you to what sort of party?',
+        option1: 'Frat',
+        option2: 'Tea',
+        option3: 'New Year Eve',
+        answer:2
+    },
+
+    {
+        question: 'Who killed Dumbledore?',
+        option1: 'Harry Potter',
+        option2: 'Hagrid',
+        option3: 'Snape',
+        answer:3
+    },
+]
