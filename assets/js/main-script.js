40ae5aca196ce353d603c155f96189dfd296e8d0
cmjimerson1s <cmjimerson1s@gmail.com>
2022-12-09 08:05:24 +0000

    Add basic functionality of the enemy attack

--- assets/js/main-script.js
+++ assets/js/main-script.js
@@ -15,25 +15,35 @@ function fighterAttack() {
     let warriorRoll = document.getElementById('warrior-dice-value').textContent = num1;
         if(warriorRoll >= acRateGoblin) {
             let num2 = Math.floor(Math.random() * 12) + 1;
-            let fighterRoundDmg = document.getElementById('warrior-dmg-value').textContent = num2;
-            roundResult();
+            document.getElementById('warrior-dmg-value').textContent = num2;
+            fighterRoundResult();
             let roundDmg = document.getElementById('enemy-health-value').innerHTML
             setTimeout(() => alert('You hit! You did ' +num2+ ' damage! The goblin only has ' +roundDmg+ ' health left!'), 500);
             roundUp();
         } else {
-            alert('You missed! Now it is the enemies turn!');
+            setTimeout(() => alert('You missed! Now it is the enemies turn!'), 500);
             roundUp();
+            enemyAttack();
         }
 }
 
 function enemyAttack() {
-    
+    let num1 = Math.floor(Math.random() * 20) + 1;
+    let goblinRoll = document.getElementById('enemy-attk-roll').textContent = num1;
+        if(goblinRoll >= acRate) {
+            let num3 = Math.floor(Math.random() * 6) + 1;
+            document.getElementById('enemy-hit-value').textContent = num3;
+        }
 }
 
-function roundResult ( ) {
+function fighterRoundResult() {
 
     let roundDmg = (Math.max(0, ((parseInt(document.getElementById('enemy-health-value').innerText)) - (parseInt(document.getElementById('warrior-dmg-value').innerText)))));
     document.getElementById('enemy-health-value').innerHTML = roundDmg;
+}
+
+function goblinRoundResult() {
+
 }
 
 function roundUp () {
