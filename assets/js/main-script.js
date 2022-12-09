852665fc0e027826c6bfb09d227bf9c1ceab58d5
cmjimerson1s <cmjimerson1s@gmail.com>
2022-12-09 06:32:13 +0000

    Add gameStart function and event listener

--- assets/js/main-script.js
+++ assets/js/main-script.js
@@ -1,2 +1,10 @@
 const acRate = 16;
 const acRateGoblin = 8;
+
+let round = parseInt(document.getElementById('round-count').innerText);
+
+document.addEventListener('DOMContentLoaded', gameStart);
+
+function gameStart () {
+    let round = 0;
+}
